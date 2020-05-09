f51a2185abf1342f6a0286d74235006b876f1d83
unknown <610541@CS.MUM.EDU>
2020-05-09 14:27:14 -0500

    second test log

--- src/test/java/com/example/bookdirectory/BookDirectoryApplicationTests.java
+++ src/test/java/com/example/bookdirectory/BookDirectoryApplicationTests.java
@@ -14,6 +14,7 @@ class BookDirectoryApplicationTests {
 	@Test
 	void contextLoads() {
 		logger.info("Test case executing....");
+		logger.info("Test case executing second log statement...");
 		assertEquals(true,true);
 	}
 
